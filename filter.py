b7626a165451ee336656b9573b95261268d9aa2b
Evgeniy <evgeniy-strel@yandex.ru>
2021-11-20 20:33:56 +0500

    2 этап

--- filter.py
+++ filter.py
@@ -1,28 +1,38 @@
 from PIL import Image
 import numpy as np
+
+class Mozabrick:
+    def __init__(self, pixels, size, step):
+        self.pixels = pixels;
+        self.height = len(pixels);
+        self.width = len(pixels[1]);
+        self.size = size;
+        self.step = step;
+
+    def image_processing(self):
+        for i in range(0, self.height, self.size):
+            for j in range(0, self.width, self.size):
+                medium_brightness = self.get_medium_brightness(i, j);
+                self.color_pixels(i, j, medium_brightness);
+        return Image.fromarray(self.pixels);
+
+    def color_pixels(self, currentI, currentJ, medium_brightness):
+        for x in range(currentI, currentI + self.size):
+            for y in range(currentJ, currentJ + self.size):
+                for channel in range(3):
+                    self.pixels[x][y][channel] = medium_brightness // self.step * self.step // 3;
+
+    def get_medium_brightness(self, currentI, currentJ):
+        sumRgb = 0;
+        for x in range(currentI, currentI + self.size):
+            for y in range(currentJ, currentJ + self.size):
+                pixel = self.pixels[x][y];
+                sumRgb += sum(pixel);
+
+        return int(sumRgb // (self.size ** 2));
+
+
 img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a:
-    j = 0
-    while j < a1:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                red = arr[n][n1][0]
-                green = arr[n][n1][1]
-                blue = arr[n][n1][2]
-                M = int(red) + int(green) + int(blue)
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50 // 3
-                arr[n][n1][1] = int(s // 50) * 50 // 3
-                arr[n][n1][2] = int(s // 50) * 50 // 3
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
+pixels = np.array(img)
+result = Mozabrick(pixels, 10, 50).image_processing();
+result.save('res.jpg')
