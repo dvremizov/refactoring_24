b0e28d6b2c4c96d244907c14f520ba5dfd95eafc
n-dub <ragucoz@gmail.com>
2021-11-20 16:38:14 +0500

    Step 1

--- filter.py
+++ filter.py
@@ -5,23 +5,22 @@ arr = np.array(img)
 a = len(arr)
 a1 = len(arr[1])
 i = 0
-while i < a - 11:
+while i < a:
     j = 0
-    while j < a1 - 11:
+    while j < a1:
         s = 0
         for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = n1 + n2 + n3
-                s += M
+            for k in range(j, j + 10):
+                r = arr[n][k][0]
+                g = arr[n][k][1]
+                b = arr[n][k][2]
+                s += int(r) + int(g) + int(b)
         s = int(s // 100)
         for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
+            for k in range(j, j + 10):
+                arr[n][k][0] = int(s // 50) * 50 // 3
+                arr[n][k][1] = int(s // 50) * 50 // 3
+                arr[n][k][2] = int(s // 50) * 50 // 3
         j = j + 10
     i = i + 10
 res = Image.fromarray(arr)
