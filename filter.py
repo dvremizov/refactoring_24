8bd23cc969e162102f130428fb369d85766f9de3
n-dub <ragucoz@gmail.com>
2021-11-20 17:00:12 +0500

    Step 2

--- filter.py
+++ filter.py
@@ -1,27 +1,41 @@
 from PIL import Image
 import numpy as np
-img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a:
-    j = 0
-    while j < a1:
-        s = 0
-        for n in range(i, i + 10):
-            for k in range(j, j + 10):
-                r = arr[n][k][0]
-                g = arr[n][k][1]
-                b = arr[n][k][2]
-                s += int(r) + int(g) + int(b)
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for k in range(j, j + 10):
-                arr[n][k][0] = int(s // 50) * 50 // 3
-                arr[n][k][1] = int(s // 50) * 50 // 3
-                arr[n][k][2] = int(s // 50) * 50 // 3
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
+
+GRAYSCALE_STEP_COUNT = 4
+GRAYSCALE_STEP = 255 / GRAYSCALE_STEP_COUNT
+SIZE = 5
+
+
+def calc_average(arr, x, y):
+    s = 0
+    for n in range(x, x + SIZE):
+        for k in range(y, y + SIZE):
+            r = arr[n][k][0]
+            g = arr[n][k][1]
+            b = arr[n][k][2]
+            s += int(r) + int(g) + int(b)
+    return int(s // SIZE ** 2)
+
+
+def calc_grayscale(arr, x, y, color):
+    for n in range(x, x + SIZE):
+        for k in range(y, y + SIZE):
+            arr[n][k][0] = color
+            arr[n][k][1] = color
+            arr[n][k][2] = color
+
+
+def main():
+    source_img = Image.open('img2.jpg')
+    source_arr = np.array(source_img)
+    for i in range(0, len(source_arr), SIZE):
+        for j in range(0, len(source_arr[1]), SIZE):
+            s = calc_average(source_arr, i, j)
+            color = int(s // GRAYSCALE_STEP) * GRAYSCALE_STEP // 3
+            calc_grayscale(source_arr, i, j, color)
+    res = Image.fromarray(source_arr)
+    res.save('res.jpg')
+
+
+if __name__ == '__main__':
+    main()
