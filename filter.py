f261be1b033e1b64b9b9c3713b099a705303dade
nech <dhdhhd.cx@gmail.com>
2021-11-21 18:47:10 +0500

    3 этап

--- filter.py
+++ filter.py
@@ -2,37 +2,32 @@ from PIL import Image
 import numpy as np
 
 
-def mosaic(image, mosaic_modifier, mosaic_step):
-    img_arr = np.array(image)
-    img_height = len(img_arr)
-    img_width = len(img_arr[1])
-    height = 0
+class Mosaica:
+    def __init__(self, mosaic_size, mosaic_gradation):
+        self.image = Image.open("img2.jpg")
+        self.img_array = np.array(self.image)
+        self.img_height = len(self.img_array)
+        self.img_width = len(self.img_array[1])
+        self.mosaic_size = mosaic_size
+        self.gradation = mosaic_gradation
 
-    while height < img_height:
-        width = 0
-        while width < img_width:
-            avg_brightness = get_brightness(img_arr, height, width, mosaic_modifier)
-            modify_img_array(img_arr, avg_brightness, height, width, mosaic_modifier, mosaic_step)
-            width = width + mosaic_modifier
-        height = height + mosaic_modifier
-    res = Image.fromarray(img_arr)
-    res.save('res.jpg')
+    def mosaic(self, ):
+        for height in range(0, self.img_height, self.mosaic_size):
+            for width in range(0, self.img_width, self.mosaic_size):
+                avg_brightness = self.get_brightness(height, width)
+                self.modify_img_array(avg_brightness, height, width, self.mosaic_size, self.gradation)
 
+        res = Image.fromarray(self.img_array)
+        return res
 
-def get_brightness(image_array, height, width, modifier):
-    brightness = 0
-    for y in range(height, height + modifier):
-        for x in range(width, width + modifier):
-            brightness += int(image_array[y][x][0]) + int(image_array[y][x][1]) + int(image_array[y][x][2])
-    return int(brightness // modifier ** 2)
+    def get_brightness(self, height, width):
+        avg_brightness = self.img_array[height: height + self.mosaic_size, width:width + self.mosaic_size].sum()
+        return int(avg_brightness // self.mosaic_size ** 2)
 
-
-def modify_img_array(img_arr, avg_brightness, height, width, mosaic_modifier, mosaic_step):
-    for y in range(height, height + mosaic_modifier):
-        for x in range(width, width + mosaic_modifier):
-            img_arr[y][x][0] = int((avg_brightness // mosaic_step) * mosaic_step) / 3
-            img_arr[y][x][1] = int((avg_brightness // mosaic_step) * mosaic_step) / 3
-            img_arr[y][x][2] = int((avg_brightness // mosaic_step) * mosaic_step) / 3
+    def modify_img_array(self, avg_brightness, height, width, mosaic_modifier, mosaic_step):
+        self.img_array[height: height + mosaic_modifier, width:width + mosaic_modifier] = int(
+            (avg_brightness // mosaic_step) * mosaic_step) / 3
 
 
-mosaic(Image.open("img2.jpg"), 10, 50)
+result = Mosaica(10, 50).mosaic()
+result.save('res.jpg')
