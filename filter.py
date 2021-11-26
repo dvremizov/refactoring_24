35d51c1c6ea9102a39d0c3ab0d2e402d98340659
Lyba Lila <lila.lyba5@yandex.ru>
2021-11-26 20:32:09 +0500

    2 задание

--- filter.py
+++ filter.py
@@ -1,28 +1,39 @@
 from PIL import Image
 import numpy as np
-img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a - 1:
-    j = 0
-    while j < a1 - 1:
-        s = 0
-        for n in range(i, i + 10):
-            for n_1 in range(j, j + 10):
-                n1 = int(arr[n][n_1][0] // 3)
-                n2 = int(arr[n][n_1][1] // 3)
-                n3 = int(arr[n][n_1][2] // 3)
-                M = n1 + n2 + n3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n_1 in range(j, j + 10):
-                arr[n][n_1][0] = int(s // 50) * 50
-                arr[n][n_1][1] = int(s // 50) * 50
-                arr[n][n_1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
+
+
+def pixel_art(img, mosaic_size, step):
+    arr = np.array(img)
+    img_height = len(arr)
+    img_width = len(arr[1])
+    height = 0
+
+    while height < img_height - 1:
+        width = 0
+        while width < img_width - 1:
+            shade = give_shade(arr, mosaic_size, height, width)
+            gav(arr, shade, mosaic_size, step, height, width)
+            width = width + 10
+        height = height + 10
+    res = Image.fromarray(arr)
+    res.save('res.jpg')
+
+
+def give_shade(arr, mosaic_size, height, width):
+    shade = 0
+    for x in range(height, height + mosaic_size):
+        for y in range(width, width + mosaic_size):
+            shade += int(arr[x][y][0] // 3) + int(arr[x][y][1] // 3) + int(arr[x][y][2] // 3)
+    return int(shade // mosaic_size ** 2)
+
+
+def gav(arr, shade, mosaic_size, step, height, width):
+    for n in range(height, height + mosaic_size):
+        for n_1 in range(width, width + mosaic_size):
+            arr[n][n_1][0] = int(shade // step) * step
+            arr[n][n_1][1] = int(shade // step) * step
+            arr[n][n_1][2] = int(shade // step) * step
+
+
+pixel_art(Image.open("img2.jpg"), 10, 50)
+
