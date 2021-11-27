76e152c368d9131991d474e7759e24ec53bd474f
somesimplecore <alex.itman@mail.ru>
2021-11-27 12:09:13 +0500

    Этап 2-4

--- filter.py
+++ filter.py
@@ -1,28 +1,16 @@
 from PIL import Image
 import numpy as np
-img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a:
-    j = 0
-    while j < a1:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                red = arr[n][n1][0]
-                blue = arr[n][n1][1]
-                green = arr[n][n1][2]
-                M = int(red) + int(blue) + int(green)
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50 // 3
-                arr[n][n1][1] = int(s // 50) * 50 // 3
-                arr[n][n1][2] = int(s // 50) * 50 // 3
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
+img = Image.open(input("Введите имя исходного изображения: "))
+mosaicSize = int(input("Введите размер мозаики: "))
+grayscale = int(input("Введите желаемую градацию серого: "))
+
+def getMosaicImg(img, mosaicSize, grayscale):
+    arr = np.array(img)
+    height, width = len(arr), len(arr[1])
+    for i in range(0, height, mosaicSize):
+        for j in range(0, width, mosaicSize):
+            rgbGrey = arr[i:i + mosaicSize, j:j + mosaicSize].sum() // 100 // 3 // grayscale * grayscale
+            arr[i:i + mosaicSize, j:j + mosaicSize] = rgbGrey
+    return Image.fromarray(arr)
+
+getMosaicImg(img, mosaicSize, grayscale).save(input("Введите имя результирующего изображения: "))
